cb3ea7b2bf0427cd6948a782cb4500196379c186
kicaj <kicaj@kdev.pl>
2018-05-15 13:18:04 +0200

    Add helper

---
+++ src/View/Helper/FileHelper.php
@@ -0,0 +1,39 @@
+<?php
+namespace File\View\Helper;
+
+use Cake\View\Helper;
+
+class FileHelper extends Helper
+{
+
+    /**
+     * Load helpers
+     *
+     * @var array
+     */
+    public $helpers = [
+        'Html',
+    ];
+
+    /**
+     * Thumb image
+     *
+     * @param string|array $path Path to the image file, relative to the app/webroot/img/ direct
+     * @param array $options Array of HTML attributes. See above for special options.
+     * @param string $thumb Name of thumb
+     * @return string completed img tag
+     * @link https://book.cakephp.org/3.0/en/views/helpers/html.html#linking-to-images
+     */
+    public function thumb($path, $options = [], $thumb = 'default')
+    {
+        if ($thumb !== 'default') {
+            $path = preg_replace('/default/', $thumb, $path);
+        }
+
+        if (!is_array($options)) {
+            $options = [];
+        }
+
+        return $this->Html->image($path, $options);
+    }
+}
